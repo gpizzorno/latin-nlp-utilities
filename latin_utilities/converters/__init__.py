0b26c713690864e5ef511b5af267a5d7b288b9a5
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-11-11 09:26:09 -0500

    reorganized converters

--- latin_utilities/converters/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Interface for converters."""
-
-from .dalmepos_to_upos import dalmepos_to_upos
-from .upos_to_perseus import upos_to_perseus
-
-__all__ = [
-    'dalmepos_to_upos',
-    'upos_to_perseus',
-]
