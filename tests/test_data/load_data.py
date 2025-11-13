7da1aaa3e28dddf4f4ef8dec85b0aa96e5909acd
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-11-13 17:38:02 -0500

    path fixes and additional fixture loading

--- tests/test_data/load_data.py
+++ tests/test_data/load_data.py
@@ -5,14 +5,37 @@ from __future__ import annotations
 import json
 from pathlib import Path
 
-with Path(__file__).with_name('en_sentence.json').open(encoding='utf-8') as file:
+base_path = Path(__file__).parent
+
+with (base_path / 'en_sentence.json').open(encoding='utf-8') as file:
     EN_SENTENCE = json.load(file)
 
-with Path(__file__).with_name('la_sentence.json').open(encoding='utf-8') as file:
+with (base_path / 'la_sentence.json').open(encoding='utf-8') as file:
     LA_SENTENCE = json.load(file)
 
-with Path(__file__).with_name('upos_deprel_invalid_pairs.json').open(encoding='utf-8') as file:
+with (base_path / 'upos_deprel_invalid_pairs.json').open(encoding='utf-8') as file:
     UPOS_DEPREL_INVALID_PAIRS = json.load(file)
 
-with Path(__file__).with_name('upos_deprel_valid_pairs.json').open(encoding='utf-8') as file:
+with (base_path / 'upos_deprel_valid_pairs.json').open(encoding='utf-8') as file:
     UPOS_DEPREL_VALID_PAIRS = json.load(file)
+
+with (base_path / 'brat' / 'simple.ann').open(encoding='utf-8') as file:
+    SIMPLE_ANN = file.read()
+
+with (base_path / 'brat' / 'simple_with_root.ann').open(encoding='utf-8') as file:
+    SIMPLE_ANN_WITH_ROOT = file.read()
+
+with (base_path / 'brat' / 'simple.txt').open(encoding='utf-8') as file:
+    SIMPLE_TXT = file.read()
+
+with (base_path / 'brat' / 'simple_with_root.txt').open(encoding='utf-8') as file:
+    SIMPLE_TXT_WITH_ROOT = file.read()
+
+with (base_path / 'brat' / 'simple.conllu').open(encoding='utf-8') as file:
+    SIMPLE_CONLLU = file.read()
+
+with (base_path / 'brat' / 'multi.conllu').open(encoding='utf-8') as file:
+    MULTI_CONLLU = file.read()
+
+with (base_path / 'brat' / 'metadata.json').open(encoding='utf-8') as file:
+    BRAT_METADATA = json.load(file)
