5ce06923cd63be104c5e102143e9dffe405c1a8f
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-11-15 15:03:19 -0500

    added case normalization

--- nlp_utilities/normalizers.py
+++ nlp_utilities/normalizers.py
@@ -36,6 +36,9 @@ def normalize_features(
         output = {}
 
         for attr, value in features.items():
+            # normalize attr label: capitalize first letter
+            attr = attr.capitalize()  # noqa: PLW2901
+            # check if attr is in feature set and value is valid for UPOS
             if attr in feature_set:
                 record = feature_set[attr]
                 if upos in record['byupos'] and value in record['byupos'][upos] and record['byupos'][upos][value] != 0:
@@ -65,5 +68,10 @@ def normalize_xpos(upos: str, xpos: str) -> str:
     new_xpos = ''
     for i, val in enumerate(xpos[1:], start=2):
         new_xpos = new_xpos + val if upos_tag in VALIDITY_BY_POS.get(i, '') else new_xpos + '-'
+
+    # ensure lowercase
+    new_xpos = new_xpos.lower()
+    # ensure correct length
+    new_xpos = new_xpos + '-' * (8 - len(new_xpos))
 
     return f'{upos_tag}{new_xpos}'
